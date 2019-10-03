3061ebaf901af47ff7c4c473411202caffeef05c
James Tang <jytang10@gmail.com>
2019-10-02 21:35:53 -0700

    Updates

---
+++ precache-manifest.2a47375fd8c73ed8cfcf30702754c4d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6c42d8e1e50fe88903bfadde6622e8",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "9885c52e3c406f144151",
+    "url": "/react-movie-app/static/css/main.21eb1eed.chunk.css"
+  },
+  {
+    "revision": "717b4a07f3e1df9b25b0",
+    "url": "/react-movie-app/static/js/2.c8d7adf8.chunk.js"
+  },
+  {
+    "revision": "9885c52e3c406f144151",
+    "url": "/react-movie-app/static/js/main.4a35a602.chunk.js"
+  },
+  {
+    "revision": "c9688c5455a3058700ab",
+    "url": "/react-movie-app/static/js/runtime-main.0f54eed5.js"
+  }
+]);
