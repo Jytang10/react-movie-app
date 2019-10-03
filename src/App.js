7e5ce1f9ccd3548bdea4f9fa3bb49e0bd16fa94d
James Tang <jytang10@gmail.com>
2019-10-02 20:46:54 -0700

    Created Movie component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import axios from "axios";
 class App extends React.Component {
   state = {
     isLoading: true,
-    movie: []
+    movies: []
   };
 
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
   }
 
   componentDidMount() {
